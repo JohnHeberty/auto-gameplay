f7dd0078b80b0d691c7827c31918ef8e4954bb2a
John Heberty de Freitas <46422955+JohnHeberty@users.noreply.github.com>
2025-06-23 21:00:47 -0300

    add: scripts restore db

---
+++ src/scripts/restore_db.sh
@@ -0,0 +1,5 @@
+docker exec -u root -it auto-gameplay-db bash
+chmod -R 777 /backups
+exit
+docker exec -it auto-gameplay-db bash
+psql -U usergameplay -d autogameplay -f /backups/backup-2025-06-23.sql
