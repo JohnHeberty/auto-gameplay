123f7aaba345f7814be7e8ae53551b77668f9dc1
John Heberty de Freitas <46422955+JohnHeberty@users.noreply.github.com>
2025-07-11 13:24:46 -0300

    add: pipeline proplayers and move views and update backup

--- src/repository/querys/init/create/table_5_proplayers.sql
+++ src/repository/querys/init/create/table_5_proplayers.sql
@@ -1,14 +1,16 @@
 CREATE TABLE IF NOT EXISTS PROPLAYERS (
     ID_PROPLAYER SERIAL,
-    "NOME" VARCHAR(50) NOT NULL,
-    "description" VARCHAR(1000),
-    REGISTER_DATE DATE NOT NULL,
+    ID_ACCOUNT INTEGER NOT NULL UNIQUE,
+    NAME VARCHAR(50) NOT NULL,
+    DESCRIPTION VARCHAR(1000),
+    REGISTER_DATE TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (ID_PROPLAYER)
 );
 
 -- Comentários nas colunas
 COMMENT ON TABLE PROPLAYERS IS                  'Tabela para armazenar informações dos jogadores profissionais';
 COMMENT ON COLUMN PROPLAYERS.ID_PROPLAYER IS    'ID único do jogador profissional (auto-incremento)';
-COMMENT ON COLUMN PROPLAYERS."NOME" IS          'Nome do jogador profissional';
-COMMENT ON COLUMN PROPLAYERS."description" IS   'Descrição ou biografia do jogador profissional';
+COMMENT ON COLUMN PROPLAYERS.ID_ACCOUNT IS      'ID da conta associada ao jogador profissional';
+COMMENT ON COLUMN PROPLAYERS.NOME IS            'Nome do jogador profissional';
+COMMENT ON COLUMN PROPLAYERS.DESCRIPTION IS     'Descrição ou biografia do jogador profissional';
 COMMENT ON COLUMN PROPLAYERS.REGISTER_DATE IS   'Data de registro do jogador profissional no sistema';
--- src/scripts/restore_db.sh
+++ src/scripts/restore_db.sh
@@ -2,4 +2,4 @@ docker exec -u root -it auto-gameplay-db bash
 chmod -R 777 /backups
 exit
 docker exec -it auto-gameplay-db bash
-psql -U usergameplay -d autogameplay -f /backups/backup-2025-06-23.sql
+psql -U usergameplay -d autogameplay -f /backups/backup-2025-07-11.sql
